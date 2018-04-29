e34b74d44c5158f0ce9c917a07ae1cee15bf63d3
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-29 15:46:53 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,10 @@
+require 'pry'
 class Hash
   def keys_of(*arguments)
     # code goes here
     arr = Array.new
     arguments.each do |argument|
+        binding.pry
         (arr << argument) if self.keys.include? argument
     end
     arr
